27459b70f594756f702e529bc9eabb4f059d163f
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-05-23 17:12:32 -0300

    Finalizando Fetch e Promises

--- Modulo-01/012-RequisicoesHTTP/js/script.js
+++ Modulo-01/012-RequisicoesHTTP/js/script.js
@@ -4,10 +4,36 @@ window.addEventListener("load", function(){
         res.json().then(data => {
             showData(data);
         });
+    }).catch(error => {
+        console.log("Erro na requisição");
     });
+
+    executedDivisionPromise();
+    executedDivisionPromisesAsyncAwait()
+
 });
 
 function showData(data){
     const user = document.querySelector("#user");
     user.textContent = data.login + "" + data.name;
+}
+
+function executedDivisionPromise(a, b){
+    return new Promise((resolve, reject) => {
+        if (b === 0){
+            reject("Não é possível a divisão por zero")
+        }
+        resolve(a / b);
+    })
+}
+
+
+
+function executedDivisionPromisesAsyncAwait(){
+    const division = await executedDivisionPromise(12,2);
+    console.log(division);
+
+ 
+
+
 }
--- Modulo-01/012-RequisicoesHTTP/requisicoesHttp.md
+++ Modulo-01/012-RequisicoesHTTP/requisicoesHttp.md
@@ -7,4 +7,12 @@ O comando **fetch** é o padrão para se realizar requisições HTTP, o que sign
 Este comando tem dois retorno, o primeiro recebe os dados em formato binário, e em seguida é executada uma função interna com estes dados binários para a conversão em JSON, que retorna uma nova promisse. 
 
 
+## Promises
+
+É uma construção usada pelo JS e outras linguagens que é uma promessa de que algo será resolvido no futuro, ou seja, uma promessa de execução que pode ou não ser cumprida. Quando for resolvida ela será **ok** - then, sendo capturada, e quando é rejeitada acarretará **erro** - catch.
+
+## Async/await
+
+É uma espécie de açucar sintático que promove a ideia de código síncrono, melhorando a legibilidade do código. 
+
 
