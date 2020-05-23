1906ad7e0b647d425d85777b328274a478ff2ff5
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-05-23 15:26:13 -0300

    Refatorando - Aplicando JS Moderno

---
+++ Modulo-01/010-Refatoracao/js/scripts.js
@@ -0,0 +1,126 @@
+let globalNames = ["Um", "Dois", "Três", "Quatro", "Cinco"];
+let inputName = null;
+let currentIndex = null;
+let isEditing = false;
+
+
+window.addEventListener("load",() =>{
+
+    inputName = document.querySelector("#inputName");
+
+    preventformSubmit();
+    activeInput();
+    remover();
+})
+
+function preventFromSubmit(){
+    function handleFormSubmit(event){
+        event.preventDefault();
+    }
+
+    var form = document.querySelector("form");
+    form.addEventListener("submit", handleFormSubmit);
+}
+
+function activeInput(){
+    function insertName(newName){
+        globalNames = [...globalNames, newName];
+    }
+}
+
+function updateName(newName){
+    globalNames[currentIndex] = newName;
+}
+
+    
+function handleTyping(event){
+
+    var hasText = !!event.target.value && event.target.value.trim() !== "";
+
+
+    if(event.key === "Enter"){
+        if(isEditing){
+            updateName(event.target.value);
+        }
+        else{
+            insertName(event.target.value);
+        }
+        render();
+        isEditing = false;
+        clearInput();            
+    }
+}
+
+inputName.addEventListener("keyup", handleTyping);
+inputName.focus();
+
+
+function render(){
+
+function createDeleteButton(index){
+
+    function deleteName(){
+        globalNames = globalNames.filter((name, i) => {
+            if(i === index){
+                return false;
+            }
+            return true;
+        });
+        render();
+    }
+
+    var button = document.createElement("button");
+    button.classList.add("deleteButton");
+    button.textContent = "x";
+
+    button.addEventListener("click", deleteName);
+
+    return button;
+}
+
+function createSpan(nome, index){
+
+    function editItem(){
+        inputName.value = nome;
+        inputName.focus();
+        isEditing = true;
+        currentIndex = index;
+    }
+
+    var span = document.createElement("span");
+    span.classList.add("clickable");
+    span.textContent = nome;
+    span.addEventListener("click", editItem);
+
+    return span;
+}
+
+var divNames = document.querySelector("#nomes");
+divNames.innerHTML = "";
+
+// CRIAR UL
+// FAZER N LIS, CONFORME TAMANHO DO VETOR GLOBANAMES
+
+var ul = document.createElement("ul");
+
+for (var i = 0; i < globalNames.length; i++){
+    var currentName = globalNames[i];
+
+    var li = document.createElement("li");
+    var button = createDeleteButton(i);
+    var span = createSpan(currentName);
+
+    li.appendChild(button);
+    li.appendChild(span);
+    ul.appendChild(li);
+}
+
+divNames.appendChild(ul);
+clearInput();
+
+}
+
+function clearInput(){
+inputName.value = "";
+inputName.focus();
+}
