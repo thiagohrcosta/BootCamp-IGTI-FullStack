e88154ad2aca179fd33d1e5747de8b2bc9b6df2f
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-04 11:27:38 -0300

    Criando método POST parte 2  My Bank App

--- Modulo-02/My-Bank-API/index.js
+++ Modulo-02/My-Bank-API/index.js
@@ -1,6 +1,9 @@
 var express = require("express");
+var fs = require("fs");
 
 var app = express();
+
+app.use(express.json());
 
 app.get("/", function(req, res){
     res.send("Hello World")
@@ -8,8 +11,14 @@ app.get("/", function(req, res){
 });
 
 app.post("/account", (req, res) => {
-    console.log("Post Account")
-})
+    let params = req.body;
+
+    fs.appendFile("accounts.json", JSON.stringify(params), err => {
+        console.log(err);
+    });
+    
+    res.send("post account");
+});
 
 app.listen(3333, function(){
     console.log("API Started");
