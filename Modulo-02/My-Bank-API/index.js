d36589dd1ebb9e03a8795894a2d9f9ca7184c336
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-03 22:08:44 -0300

    Iniciando servidor com Express My Bank API

---
+++ Modulo-02/My-Bank-API/index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res){
+    res.send("Hello World");
+
+});
+
+app.listen(3333, function(){
+    console.log("API Started");
+});
