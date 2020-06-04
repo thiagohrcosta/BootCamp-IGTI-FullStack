8f234a19461af03c5ee30b0668ae48a71443f4ca
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-04 16:37:58 -0300

    Finalizando aula 5 - My Bank App

--- Modulo-02/My-Bank-API/index.js
+++ Modulo-02/My-Bank-API/index.js
@@ -6,29 +6,53 @@ var app = express();
 app.use(express.json());
 
 app.post("/account", (req, res) => {
-    let params = req.body;
+    let account = req.body;
 
     fs.readFile("accounts.json", "utf8", (err, data) =>{
-        console.log(err);
+        if(err){
+            try{
+                let json = JSON.parse(data);
+                account = { id: json.nextId++, ...account };               
+                json.accounts.push(account);
 
-        try{
-            let json = JSON.parse(data);
-            console.log(json);
-            
-            res.send("post account");
+                fs.writeFile("accounts.json", JSON.stringify(json), err => {
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        res.end();
+                    }
+                 });
+            }
+            catch(err){
+                res.status(400).send({ errpr: err.message });
+            }            
         }
-        catch(err){
-            res.send("erro");
+        else{
+            res.status(400).send({ error: err.message });
         }
-    });
 
-    /*
-    fs.appendFile("accounts.json", JSON.stringify(params), err => {
-        console.log(err);
     });
-    */
 });
 
 app.listen(3333, function(){
+
+    try{
+        fs.readFile("accounts.json", "utf8", (err, data) =>{
+           if(err){
+               const initialJson = {
+                    nextId: 1,
+                    accounts: []
+               };
+               fs.writeFile("accounts.json", JSON.stringify(initialJson), err => {
+                   console.log(err);
+               });
+           }
+        });
+
+    }
+    catch (err){
+        console.log(err);
+    }
     console.log("API Started");
 });
