ae69f67562092f4e3afe8c9214eb2f0ddaf872d5
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-03 22:33:51 -0300

    Finalizando método POST parte 1

--- Modulo-02/My-Bank-API/index.js
+++ Modulo-02/My-Bank-API/index.js
@@ -3,9 +3,13 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-    res.send("Hello World");
+    res.send("Hello World")
 
 });
+
+app.post("/account", (req, res) => {
+    console.log("Post Account")
+})
 
 app.listen(3333, function(){
     console.log("API Started");
