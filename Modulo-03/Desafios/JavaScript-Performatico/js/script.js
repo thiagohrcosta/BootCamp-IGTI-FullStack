eda2ce03c8316bb92363ac49180e57aee6f91857
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-13 12:48:31 -0300

    javascript performático

--- Modulo-03/Desafios/JavaScript-Performatico/js/script.js
+++ Modulo-03/Desafios/JavaScript-Performatico/js/script.js
@@ -8,19 +8,32 @@ function start(){
 }
 
 function handleButtonClick(){
-   clickArray.push(getNewTimestamp());
+    const item = getNewTimestamp();
+    clickArray.push(item);
 
-    render();
+    render(item);
 }
 
-function render(){
+function render(item){
     const ul = document.querySelector('#data');
+
+    const li = document.createElement('li');
+    li.textContent = item;
+
+    ul.appendChild(li);
+    
+    /*
+    
     ul.innerHTML = '';
 
     let lis = '';
     
-    clickArray.map(item => {
+    clickArray.map((item) => {
         lis += `<li>${item}</li>`;
     });
     ul.innerHTML = lis;
+
+    */
+
+    document.title = clickArray.length;
 }
