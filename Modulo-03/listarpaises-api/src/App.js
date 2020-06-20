b4b47f93482ecf2f202e2dca107e0f1dcb12f02a
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-20 14:49:11 -0300

    Finalizando parte 1 do desafio guiado Listar Paises

--- Modulo-03/listarpaises-api/src/App.js
+++ Modulo-03/listarpaises-api/src/App.js
@@ -9,7 +9,34 @@ export default class App extends Component {
     allCountries: [],
   };
 }
+
+  async componentDidMount(){
+
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const json = await res.json();
+
+    const allCountries = json.map(({name, numericCode, flag, population }) => {
+      return {
+        id: numericCode,
+        name,
+        flag,
+        population
+      };
+    }); 
+
+    this.setState({
+      allCountries,
+    });
+  }
   render(){
-    return <h1>React Contries</h1>;
+    const { allCountries } = this.state;
+
+    console.log(allCountries);
+    
+    return (
+      <div className="container">
+        <h1>React Contries</h1>
+      </div>
+    );    
   }
 }
