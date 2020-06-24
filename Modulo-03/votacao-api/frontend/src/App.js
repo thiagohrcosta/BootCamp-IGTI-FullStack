c4f448b29484058a21d9fe2caa0af3e71d47ed37
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-24 15:08:19 -0300

    Testando backend e frontend da API votos

--- Modulo-03/votacao-api/frontend/src/App.js
+++ Modulo-03/votacao-api/frontend/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      candidates: [],
+    };
+
+    this.interval = null;
+  }
+
+  componentDidMount(){
+    this.interval = setInterval(() =>{
+      fetch('http://localhost:8080/votes')
+      .then(res => {
+        return res.json()
+        .then((json) =>{
+          this.setState({
+            candidates: json.candidates,
+          });
+        })
+      })
+    }, 1000);
+  }
+
+  render() {
+    const { candidates } = this.state;
+
+    if(candidates.length === 0){
+      return <span>Carregando...</span>
+    }
+    return <span>Candidatos</span>
+    
+  }
 }
-
-export default App;
