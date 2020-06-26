e96d98a32db8f9dcbe88d295915ddeed80ed878c
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-26 17:56:07 -0300

    Adicionando tela de loading na API e expondo dados em tela

--- Modulo-03/votacao-api/frontend/src/App.js
+++ Modulo-03/votacao-api/frontend/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
@@ -11,27 +11,48 @@ export default class App extends Component {
     this.interval = null;
   }
 
-  componentDidMount(){
-    this.interval = setInterval(() =>{
-      fetch('http://localhost:8080/votes')
-      .then(res => {
-        return res.json()
-        .then((json) =>{
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      fetch("http://localhost:8080/votes").then((res) => {
+        return res.json().then((json) => {
           this.setState({
             candidates: json.candidates,
           });
-        })
-      })
+        });
+      });
     }, 1000);
   }
 
   render() {
     const { candidates } = this.state;
 
-    if(candidates.length === 0){
-      return <span>Carregando...</span>
+    if (candidates.length === 0) {
+      return (
+        <div className="preloader-wrapper big active">
+          <div className="spinner-layer spinner-blue-only">
+            <div className="circle-clipper left">
+              <div className="circle"></div>
+            </div>
+            <div className="gap-patch">
+              <div className="circle"></div>
+            </div>
+            <div className="circle-clipper right">
+              <div className="circle"></div>
+            </div>
+          </div>
+        </div>
+      );
     }
-    return <span>Candidatos</span>
-    
+    return (
+      <div>
+        {candidates.map(({id, name, votes})=>{
+          return (
+            <p key={id}>
+              {name} - {votes}
+            </p>
+          );
+        })}          
+      </div>
+      )
   }
 }
