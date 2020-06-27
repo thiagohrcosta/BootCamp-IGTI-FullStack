5ac99cd4153dd893f6c4bafe2248b802ed066929
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-06-27 12:58:47 -0300

    Usando react-flip-move no API votações

--- Modulo-03/votacao-api/frontend/src/components/Candidates.js
+++ Modulo-03/votacao-api/frontend/src/components/Candidates.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Card from './Card';
 import Candidate from './Candidate';
+import FlipMove from 'react-flip-move';
 
 export default function Candidates({candidates}) {
   return (
     <div>
+      <FlipMove>
          {candidates.map((candidate, index) => {
            const {id} = candidate;
 
           return (
-            <Card key={id}>
+            <div key={id} >
+            <Card>
               <Candidate candidate={candidate} position={index +1}/>
             </Card>
+            </div>
           );
         })}      
+      </FlipMove>
     </div>
   )
 }
